39bd36a86a0c1495f7d4f56d7ba6a19fcd715661
Elom Amouzou <elom1990@yahoo.com>
2020-02-14 11:17:26 -0700

    add movie model test for instance methods

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -15,12 +15,25 @@ RSpec.describe Movie, type: :model do
   end
 
   before(:each) do
+    @astudios = Studio.create(name: "astudios")
+    @cstudios = Studio.create(name: "cstudios")
+    @thrillermovie = @astudios.movies.create(name: "thrillermovie", creation_year: 1975, genre: "thriller")
+    @actionmovie = @cstudios.movies.create(name: "actionmovie", creation_year: 1985, genre: "action")
+    @actor1 = Actor.create(name: "Thomas Ngijol", age: 41)
+    @actor2 = Actor.create(name: "Fabrice Eboue", age: 42)
+    @actor3 = Actor.create(name: "Aissa Maiga", age: 44)
 
+    ActorMovie.create(actor_id: @actor1.id, movie_id: @thrillermovie.id)
+    ActorMovie.create(actor_id: @actor2.id, movie_id: @actionmovie.id)
+    ActorMovie.create(actor_id: @actor3.id, movie_id: @actionmovie.id)
+    ActorMovie.create(actor_id: @actor3.id, movie_id: @thrillermovie.id)
   end
 
-  it ".gets_actors"  do
+  it ".get_sorted_actors"  do
+    expect(@thrillermovie.get_sorted_actors).to eq([@actor1, @actor3])
   end
 
-  it ".avg_age" do
+  it ".actors_avg_age" do
+    expect(@actionmovie.actors_avg_age).to eq(43)
   end
 end
