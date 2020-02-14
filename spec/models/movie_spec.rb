6f42e2e4018cc96c1c49ee94de4b74827532521a
Elom Amouzou <elom1990@yahoo.com>
2020-02-14 09:27:17 -0700

    add movie model test

---
+++ spec/models/movie_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+
+RSpec.describe Movie, type: :model do
+  describe "validations" do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :creation_year}
+    it {should validate_presence_of :genre}
+  end
+
+  describe "relationships" do
+    it {should belong_to :studio}
+    it {should have_many :actor_movies}
+    it {should have_many(:actors).through(:actor_movies)}
+  end
+
+  before(:each) do
+
+  end
+
+  it ".gets_actors"  do
+  end
+
+  it ".avg_age" do
+  end
+end
