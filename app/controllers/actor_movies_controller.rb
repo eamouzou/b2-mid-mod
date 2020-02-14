828e0300e22804118d1603c027f7e920a4a534bd
Elom Amouzou <elom1990@yahoo.com>
2020-02-14 11:54:16 -0700

    make instance variable regular variable; all tests pass

--- app/controllers/actor_movies_controller.rb
+++ app/controllers/actor_movies_controller.rb
@@ -1,7 +1,7 @@
 class ActorMoviesController < ApplicationController
   def create
-    @actor = Actor.where(name: params[:actor_name])
-    ActorMovie.create(actor_id: @actor.first.id, movie_id: params[:movie_id])
+    actor = Actor.where(name: params[:actor_name])
+    ActorMovie.create(actor_id: actor.first.id, movie_id: params[:movie_id])
     redirect_to "/movies/#{params[:movie_id]}"
   end
 end
